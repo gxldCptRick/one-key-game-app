d3419fb600d307628723e19f3e840e1e89499c22
Fermin Sandoval <ferminsandoval950@hotmail.com>
2021-09-17 14:00:57 -0600

    Updating the chat server to work differently

--- server/src/game/ioServer.ts
+++ server/src/game/ioServer.ts
@@ -1,33 +1,44 @@
 import * as http from 'http'
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+
+interface NameDict {
+    [key: string]: string | undefined
+}
+
+const names: NameDict = {}
+
+const getName = (client: Socket) => {
+    return names[client.id] || client.id
+}
+const setName = (client: Socket, name: string | undefined) => {
+    names[client.id] = name
+}
+
+const systemMessage = (io: Server, message: string) => {
+    io.emit('chat message', `Server - ${message}`)
+}
+
+const configureChatServer = (io: Server, client: Socket) => { 
+    console.log('Someone connected :(');
+    client.on('disconnect', () => {
+        console.log(`client: ${getName(client)} has disconnected`);
+        systemMessage(io, `${getName(client)} has left the chat`)
+    });
+    
+    client.on('set name', (name) => {
+        setName(client, name);
+    })
+
+    client.on('chat message', (msg) => {
+        console.log(`Recieved: ${msg} from client: ${client.id}`);
+        io.emit('chat message', `${getName(client)} - ${msg}`);
+    });
+}
 
 export const configureIoServer = (httpServer: http.Server) => {
-    let countDown = 0;
-    let intervalId: NodeJS.Timer;
     const io = new Server(httpServer);
     io.on('connection', (socket) => {
-        console.log('Someone connected :(');
-        socket.on('disconnect', () => {
-            console.log('Someone disconnected :)');
-        });
-    
-        socket.on('chat message', (msg) => {
-            console.log(`Person said ${msg}`);
-            if (msg == 'start game') {
-                countDown = 10;
-                let localId = intervalId = setInterval(() => {
-                    io.emit('chat message', --countDown);
-    
-                    if (countDown === 0) {
-                        clearInterval(localId);
-                        io.emit('game over');
-                    }
-                }, 1000)
-            } else if (msg == 'stop game') {
-                intervalId && clearInterval(intervalId);
-            }
-    
-            io.emit('chat message', `${socket.id} - ${msg}`);
-        });
+        systemMessage(io, `${getName(socket)} has joined the chat`)
+        configureChatServer(io, socket);
     });
 };
