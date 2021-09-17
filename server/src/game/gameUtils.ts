f00207cc12ce90b5ea92e5a432a2652a364eda04
Fermin Sandoval <ferminsandoval950@hotmail.com>
2021-09-17 14:47:49 -0600

    Updating the structure of the server app

---
+++ server/src/game/gameUtils.ts
@@ -0,0 +1,14 @@
+import {Socket} from 'socket.io'
+interface NameDict {
+    [key: string]: string | undefined
+}
+
+
+const names: NameDict = {}
+
+export const getName = (client: Socket) => {
+    return names[client.id] || client.id
+}
+export const setName = (client: Socket, name: string | undefined) => {
+    names[client.id] = name
+}
--- server/src/game/ioServer.ts
+++ server/src/game/ioServer.ts
@@ -1,44 +1,13 @@
 import * as http from 'http'
 import { Server, Socket } from 'socket.io';
-
-interface NameDict {
-    [key: string]: string | undefined
-}
-
-const names: NameDict = {}
-
-const getName = (client: Socket) => {
-    return names[client.id] || client.id
-}
-const setName = (client: Socket, name: string | undefined) => {
-    names[client.id] = name
-}
-
-const systemMessage = (io: Server, message: string) => {
-    io.emit('chat message', `Server - ${message}`)
-}
-
-const configureChatServer = (io: Server, client: Socket) => { 
-    console.log('Someone connected :(');
-    client.on('disconnect', () => {
-        console.log(`client: ${getName(client)} has disconnected`);
-        systemMessage(io, `${getName(client)} has left the chat`)
-    });
-    
-    client.on('set name', (name) => {
-        setName(client, name);
-    })
-
-    client.on('chat message', (msg) => {
-        console.log(`Recieved: ${msg} from client: ${client.id}`);
-        io.emit('chat message', `${getName(client)} - ${msg}`);
-    });
-}
+import { configureChatClient, systemMessage } from './chatServer';
+import { CHAT_MESSAGE, SET_NAME } from './events';
+import { getName, setName } from './gameUtils';
 
 export const configureIoServer = (httpServer: http.Server) => {
     const io = new Server(httpServer);
     io.on('connection', (socket) => {
-        systemMessage(io, `${getName(socket)} has joined the chat`)
-        configureChatServer(io, socket);
+        configureChatClient(io, socket);
+        socket.on(SET_NAME,  (name: string) => setName(socket, name))
     });
 };
