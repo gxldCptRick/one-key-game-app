8228edabf4daaf3df11d4ca6118a68b893ed7ac3
Andres Hermilo Carrera <andreshcar@live.com>
2021-09-17 23:45:44 -0600

    Enabling game server in socket.io server

--- server/src/game/ioServer.ts
+++ server/src/game/ioServer.ts
@@ -2,12 +2,14 @@ import * as http from 'http'
 import { Server } from 'socket.io';
 import { configureChatClient } from './chatServer';
 import { SET_NAME } from './events';
+import { configureGameServer } from './gameServer';
 import { setName } from './gameUtils';
 
 export const configureIoServer = (httpServer: http.Server) => {
     const io = new Server(httpServer);
     io.on('connection', (socket) => {
         configureChatClient(io, socket);
-        socket.on(SET_NAME,  (name: string) => setName(socket, name))
+        socket.on(SET_NAME,  (name: string) => setName(socket, name));
+        configureGameServer(io, socket);
     });
 };
